19eb592a5f77a8e69743c1c0d160c3201203001c
hasone1212 <101915299+hasone1212@users.noreply.github.com>
2024-11-06 09:10:32 -0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+ $servername 'localhost'; 
+ $username = 'root'; 
+$password''; 
+$dbname 'information_schema'; 
+$conn-new mysql($servername, $username, $password, $dbname); 
+$conn->set_charset('utf8');
+
+
+ ?>
